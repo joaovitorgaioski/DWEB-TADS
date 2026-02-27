581d7b90d0a3aa6c944f9f421f9afc651dcb1ee4
João Vitor Gaioski <jvgaioskI@hotmail.com>
2026-02-27 16:11:51 -0300

    ex15 - ex17

---
+++ DWEB-2/Aula01_Exercicios_Basicos_PHP/ex16.php
@@ -0,0 +1,18 @@
+<?php
+
+$n = $argv[1];
+$r = 1;
+
+if ($n >= 0){
+    for ($i = $n; $i > 0; $i--) {
+        $r = $i * $r;
+    }
+
+    echo "Fatorial de " . $n . " é " . $r;
+}
+else{
+    echo "Insira um número positivo!";
+}
+
+
+?>
