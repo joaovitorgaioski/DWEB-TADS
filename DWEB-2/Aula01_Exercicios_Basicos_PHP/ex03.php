0a9d12e2875bc2d71d48f6cc65e822676fa2cd25
João Vitor Gaioski <140276793+joaovitorgaioski@users.noreply.github.com>
2026-02-27 19:34:39 -0300

    Operador ternário

--- DWEB-2/Aula01_Exercicios_Basicos_PHP/ex03.php
+++ DWEB-2/Aula01_Exercicios_Basicos_PHP/ex03.php
@@ -6,6 +6,6 @@ $n2 = $argv[2];
 echo "Soma: " . $n1 + $n2;
 echo "\nSubtração: " . $n1 - $n2;
 echo "\nMultiplicação: " . $n1 * $n2;
-echo "\nDivisão: " . $n1 / $n2;
+echo "\nDivisão: " . ($n2!=0 ? $n1 / $n2 : "Erro: Divisão por 0!");
 
 ?>
