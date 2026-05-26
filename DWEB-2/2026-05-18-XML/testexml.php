433e693c78a9fb7dc649425fba9b7a3a954f86f3
João Vitor Gaioski <140276793+joaovitorgaioski@users.noreply.github.com>
2026-05-25 22:14:34 -0300

    Delete DWEB-2/2026-05-18-XML directory

--- DWEB-2/2026-05-18-XML/testexml.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'simplexml-data.php';
-
-$filmes = new SimpleXMLElement($xmlstr);
-
-echo $filmes->filme[0]->resumo;
-echo "<hr>";
-echo $filmes->filme[0]->personagens->personagem->nome;
