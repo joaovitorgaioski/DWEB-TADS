579739fd513fffaaa1c45bf5130d4ec5d380322f
João Vitor Gaioski <140276793+joaovitorgaioski@users.noreply.github.com>
2026-09-02 22:06:02 -0300

    Delete Exemplo4JSP directory

--- Exemplo4JSP/src/main/java/br/edu/ifpr/irati/ads/exception/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpr.irati.ads.exception;
-
-public class ServiceException extends RuntimeException {
-    public ServiceException(String message) {
-        super(message);
-    }
-}
--- Exemplo4JSP/src/main/java/br/edu/ifpr/irati/ads/exception/UrlException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpr.irati.ads.exception;
-
-public class UrlException extends RuntimeException {
-    public UrlException(String message) {
-        super(message);
-    }
-}
